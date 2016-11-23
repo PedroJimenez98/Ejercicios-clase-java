2ba215927f1604fd7a2016704ee272340d6cce11
pedrojimenez98 <pedro.makeano@hotmail.com>
2016-11-23 10:25:12 +0100

    Ejercicios actualizados

--- Ejercicios Tema 6/Ejercicio14tema6.java
+++ Ejercicios Tema 6/Ejercicio14tema6.java
@@ -7,3 +7,48 @@
 
 public class Ejercicio14tema6 {
   public static void main(String[] args) {
+    
+    int numero = 0;
+    boolean fin = false;
+    int oportunidades = 5;
+    int minimo = 0;
+    int maximo = 100;
+    int numeroPensado = 0;;
+    String respuesta = "";
+    String respuesta2 = "";
+    
+    System.out.println("Piensa un número y yo intentaré adivinarlo");
+    
+    while ((!fin) && (oportunidades > 0)) {
+      
+      numero = (int)(Math.random()*(maximo - minimo) + minimo);
+      
+      System.out.println("¿El número que piensas es " + numero + "?(si/no)");
+      
+      respuesta = System.console().readLine();
+      
+      if (respuesta.equals("si")) {
+        System.out.println("¡Si, he acertado!");
+        fin = true;
+      }
+      if (respuesta.equals("no")) {
+        if (oportunidades > 1){
+          System.out.println("¿El número que piensas es mayor o menor?");
+          respuesta2 = System.console().readLine();
+          if (respuesta2.equals("mayor")) {
+            minimo = 0;
+            minimo = numero +1;
+          }
+          if (respuesta2.equals("menor")) {
+            maximo = 100;
+            maximo = numero -1;
+          }
+        }
+        oportunidades --;
+      }
+    }
+    if (oportunidades == 0) {
+      System.out.println("¡No, he fallado!");
+    }
+  }
+}
